c0aa7edc76e7b1cce1dca9efbbe2479af84ea97f
Jeong Ju <double04j@gmail.com>
2022-12-03 14:37:08 +0900

    Update userDTO

---
+++ src/DTO/userDTO.java
@@ -0,0 +1,30 @@
+public class userDTO {
+    Integer ussn;
+    String name;
+    String phone;
+
+    public Integer getUssn() {
+        return ussn;
+    }
+
+    public void setUssn(Integer ussn) {
+        this.ussn = ussn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
