a5bc52088ba60da87c252b4f49740a3a04dfc731
Jeong Ju <double04j@gmail.com>
2022-12-03 13:49:59 +0900

    Update README

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.sql.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Connection con;
+        Statement stmt;
+        ResultSet rs;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection(
+                    "jdbc:mysql://192.168.56.3:4567/db_project",
+                    "jjlee","root");
+            stmt=con.createStatement();
+            rs=stmt.executeQuery("SELECT * FROM Book");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+" "+rs.getString(2)+
+                        " "+rs.getString(3));
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
     }
 }
