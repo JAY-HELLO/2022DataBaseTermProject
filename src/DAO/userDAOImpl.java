234beb5bc7cbf06e625d461065957c07e2f24b90
Jeong Ju <double04j@gmail.com>
2022-12-03 18:17:03 +0900

    Add user Select

--- src/DAO/userDAOImpl.java
+++ src/DAO/userDAOImpl.java
@@ -1,4 +1,64 @@
 package DAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import dbconnect.userCommon;
+import DTO.userDTO;
 
-public class userDAOImpl {
+public class userDAOImpl implements userDAO{
+
+    private userCommon uComm = userCommon.getInstance();
+    private static userDAO userdao = new userDAOImpl();
+
+    private userDAO getUserdao(){
+        return userdao;
+    }
+
+    @Override
+    public int insert(userDTO userdto) throws SQLException{
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        int ret = 0;
+
+        try{
+
+            conn = uComm.getConnection();
+            String sql = "insert into user values (?, ?, ?)";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, userdto.getUssn());
+            pstmt.setString(2, userdto.getName());
+            pstmt.setString(3,userdto.getPhone());
+
+        }finally{
+            uComm.close(pstmt, conn);
+        }
+        return ret;
+    }
+    @Override
+    public List<userDTO> select() throws SQLException{
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<userDTO> list = new ArrayList<>();
+        try{
+            conn = uComm.getConnection();
+            String sql = "select * from user";
+            pstmt = conn.prepareStatement(sql);
+
+            rs = pstmt.executeQuery();
+            while(rs.next()){
+                userDTO userdto = new userDTO();
+                userdto.setUssn(rs.getInt("ussn"));
+                userdto.setName(rs.getString("uname"));
+                userdto.setPhone(rs.getString("phone"));
+                list.add(userdto);
+            }
+        }finally{
+            uComm.close(rs,pstmt,conn);
+        }
+        return list;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
 import java.sql.*;
-public class Main {
-    public static void main(String[] args) {
+import java.util.List;
+import java.util.Scanner;
 
+import DAO.userDAOImpl;
+import DTO.userDTO;
+
+public class Main {
+    static userDAOImpl dimpl = new userDAOImpl();
+    static List<userDTO> result;
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        Scanner scan = new Scanner(System.in);
+        Integer menu = 0;
         do{
             System.out.println("///////////////////////////////////////////////");
             System.out.println("WITH Double_J 관심종목 프로그램에 오신 것을 환영합니다.");
+            System.out.println("1. 사용자 생성");
+            System.out.println("2. 사용자 목록보기");
+            System.out.println("///////////////////////////////////////////////");
 
+            menu = scan.nextInt();
+            if(menu==0){
+                break;
+            }else if(menu == 1){
 
-            System.out.println("추가할 책 id를 입력하세요 : ");
+            }else if(menu == 2){
+                result = dimpl.select();
+                for(userDTO i:result){
+                    System.out.print("사용자 고유번호 : "+i.getUssn());
+                    System.out.print("// 사용자 이름 : "+i.getName());
+                    System.out.print("// 사용자 번호 : "+i.getPhone());
+                    System.out.println(" ");
+                }
 
+            }
         }while(true);
 
     }
+    /*
+    public static List<userDTO> selectAll() throws SQLException, ClassNotFoundException {
+
+            Connection con;
+            Statement stmt;
+            ResultSet rs;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con= DriverManager.getConnection(
+                    "jdbc:mysql://192.168.56.3:4567/db_project",
+                    "jjlee","root");
+
+            stmt=con.createStatement();
+            rs=stmt.executeQuery("SELECT * FROM user");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+" "+rs.getString(2)+
+                        " "+rs.getString(3));
+            con.close();
+
+        return dimpl.select();
+    }
+    */
 }
