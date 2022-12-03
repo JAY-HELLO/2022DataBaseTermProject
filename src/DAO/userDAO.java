ba36fb3aee4b3912b31c19397a66e880c215db70
Jeong Ju <double04j@gmail.com>
2022-12-03 17:03:48 +0900

    add userDAOImpl

--- src/DAO/userDAO.java
+++ src/DAO/userDAO.java
@@ -1,4 +1,11 @@
 package DAO;
+import DTO.userDTO;
 
+import java.sql.SQLException;
+import java.util.List;
+import dbconnect.userCommon;
 public interface userDAO {
+    int insert(userDTO userDTO) throws SQLException;
+    List<userDTO> select() throws SQLException;
+
 }
